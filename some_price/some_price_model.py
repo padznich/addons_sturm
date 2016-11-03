0fd4017935509a9758202ed723c2ca0e26c2bb38
padznich <slabkopg@gmail.com>
2016-11-03 20:05:48 +0300

    [REF] logging

--- some_price/some_price_model.py
+++ some_price/some_price_model.py
@@ -5,6 +5,11 @@ from math import ceil
 import openerp.addons.decimal_precision as dp
 from openerp import api, models, fields
 
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
 
 class SomePrice(models.Model):
     _inherit = 'product.template'
@@ -31,13 +36,18 @@ class SomePrice(models.Model):
 
         if self.list_price != _price:
             self.write({'list_price': _price})
+            _logger.error('\033[1;32m{}\033[1;m'.format("." * 10 + "list_price wrote in compute_list_price"))
+            _logger.error('\033[1;32m{}\033[1;m'.format(self.seller_id.id))
+            _logger.error('\033[1;32m{}\033[1;m'.format(self.list_price) + '\tSale Price')
 
     @api.multi
     def update_list_prices(self):
+        _logger.error('\033[1;32m{}\033[1;m'.format('___start from product.template'))
 
         quantity = 1
 
         for record in self.search([('active', '=', True)]):
+            _logger.error('\033[1;32m{}\033[1;m'.format('___start iterate from product.template'))
 
             try:
                 partner_id = int(record.seller_id.id)
@@ -53,6 +63,8 @@ class SomePrice(models.Model):
             _price = ceil(pricelist.price_get(record.id, quantity, partner)[pricelist.id])
 
             record.write({'list_price': _price})
+            _logger.error('\033[1;32m{}\033[1;m'.format("=" * 10 + "list_price wrote in update_list_prices"))
+            _logger.error('\033[1;32m{}\033[1;m'.format(record.list_price) + '\tSale Price')
 
 
 class res_partner(models.Model):
@@ -60,12 +72,14 @@ class res_partner(models.Model):
 
     @api.multi
     def update_list_prices(self):
+        _logger.error('\033[1;32m{}\033[1;m'.format('___start from res.partner'))
 
         products = self.env['product.template'].search([('active', '=', True)])
 
         quantity = 1
 
         for record in products:
+            _logger.error('\033[1;32m{}\033[1;m'.format('___start iterate from res.partner'))
 
             try:
                 partner_id = int(record.seller_id.id)
@@ -81,3 +95,5 @@ class res_partner(models.Model):
             _price = ceil(pricelist.price_get(record.id, quantity, partner)[pricelist.id])
 
             record.write({'list_price': _price})
+            _logger.error('\033[1;32m{}\033[1;m'.format("+" * 10 + "list_price wrote in update_list_prices"))
+            _logger.error('\033[1;32m{}\033[1;m'.format(record.list_price) + '\tSale Price')
